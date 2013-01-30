ff1822d4676c1622eafe466fdb8bb7e87017bc1b
DafH <hugodafh@gmail.com>
2013-01-30 11:40:26 +0100

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
 
   #get "static_pages/home"
   root to: 'static_pages#home'
 
   match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
-  match '/help',  to: 'static_pages#help'
-  match '/about',  to: 'static_pages#about'
-  match '/contact',  to: 'static_pages#contact'
+  
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,5 +1,18 @@
 include ApplicationHelper
 
+def valid_signin(user)
+	fill_in "Email",    with: user.email
+ 	fill_in "Password", with: user.password
+  	click_button "Sign in"
+end
+
+RSpec::Matchers.define :have_error_message do |message|
+	match do |page|
+		page.should have_selector('div.alert.alert-error', text: message)
+	end
+end
+
+
 #def full_title(page_title)
 #	base_title = "Ruby on Rails Tutorial Sample App"
 #	if page_title.empty?
